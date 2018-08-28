032bef295ea42c469776169c1c8f6e03bcdeb03c
Nague <nagueravanely@gmail.com>
2018-08-28 16:34:12 +0100

    Ajout de sidebar et navbar ainsi que du partage des informations dans les deux sens entre appcomponent et navbarcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProduitComponent } from './produit/produit.component';
 import {ProduitMockService} from './produit/produit.mock.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  @Input()
+  showSideBar: boolean;
+
+  @Output()
+  showSideBarChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  afficherSideBar(){
+    this.showSideBar = !this.showSideBar;
+    this.showSideBarChange.emit(this.showSideBar);
+  }
+
+}
