3003f5c7845211077fc6d2e2d9b8302314fa6d76
Nague <nagueravanely@gmail.com>
2018-09-05 16:42:03 +0100

    ajout du model principal

---
+++ src/app/shared/principal.model.ts
@@ -0,0 +1,15 @@
+
+export class Principal {
+
+  constructor(
+    accountNonExpired?: boolean,
+    accountNonLocked?: boolean,
+    authorities?: any,
+    credentialsNonExpired?: boolean,
+    enabled?: boolean,
+    username?: string
+  ) {
+
+  }
+
+}
