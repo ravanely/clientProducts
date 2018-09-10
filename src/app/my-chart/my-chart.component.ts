5775f303f9c650e2edc999606a6d11b29ab0e30e
Nague <nagueravanely@gmail.com>
2018-09-10 18:34:58 +0100

    tracer de 02 graphes, l un en bar et le second lineaire

--- src/app/my-chart/my-chart.component.ts
+++ src/app/my-chart/my-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-chart',
@@ -12,4 +12,23 @@ export class MyChartComponent implements OnInit {
   ngOnInit() {
   }
 
+  @Input()
+  type = 'bar';
+
+  @Input()
+  title = 'Titre';
+
+  data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
+  options = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
 }
