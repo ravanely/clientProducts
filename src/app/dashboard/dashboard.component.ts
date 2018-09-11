0bf0604117674a6ab89bb61b1c6c037d2b628924
Nague <nagueravanely@gmail.com>
2018-09-11 13:32:21 +0100

    ajout de 02 graphes pour les utilisateurs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProduitService} from '../produit/produit.service';
+import {UserService} from '../user/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  produitsData = {
+    labels: [],
+    datasets: []
+  };
+
+  usersData = {
+    labels: [],
+    datasets: []
+  };
+
+  constructor(private produitService: ProduitService, private userService: UserService) { }
 
   ngOnInit() {
+    const datasetsQuantite = {
+      labels: 'Quantité',
+      data: [],
+      backgroundColor: 'rgb(255, 189, 64, 0.2)',
+      borderColor: 'rgb(255, 99, 132)'
+    };
+
+    const datasetsPrixUnitaire = {
+      labels: 'Prix Unitaire',
+      data: [],
+      backgroundColor: 'rgb(153, 102, 255,0.2)',
+      borderColor: 'rgb(255, 99, 132)'
+    };
+
+    this.produitService.getAll().subscribe(list => {
+      list.forEach(produit => {
+        this.produitsData.labels.push(produit.ref);
+        datasetsQuantite.data.push(produit.quantite);
+        datasetsPrixUnitaire.data.push(produit.prixUnitaire);
+      });
+    });
+
+    this.produitsData.datasets.push(datasetsQuantite);
+
+    this.produitsData.datasets.push(datasetsPrixUnitaire);
+
+    const datasetsUser = {
+      labels: 'Rôles',
+      data: [],
+      backgroundColor: 'rgb(153, 102, 255,0.2)',
+      borderColor: 'rgb(255, 99, 132)'
+    };
+
+    this.usersData.datasets.push(datasetsUser);
+
+    this.usersData.labels.push('ROLE_ADMIN');
+    this.usersData.labels.push('ROLE_USER');
+
+    this.userService.getAll().subscribe(list => {
+      let adminLength = 0;
+
+      list.forEach(user => user.roles.forEach(role => {if (role.name == 'ROLE_ADMIN') {
+            adminLength++;
+        }}));
+
+      datasetsUser.data.push(adminLength);
+
+      let userLength = 0;
+
+      list.forEach(user => user.roles.forEach(role => {if (role.name == 'ROLE_USER') {
+        userLength++;
+      }}));
+
+      datasetsUser.data.push(userLength);
+      });
   }
 
 }
--- src/app/my-chart/my-chart.component.ts
+++ src/app/my-chart/my-chart.component.ts
@@ -1,15 +1,20 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewChecked, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {ChartComponent} from 'angular2-chartjs';
 
 @Component({
   selector: 'app-my-chart',
   templateUrl: './my-chart.component.html',
   styleUrls: ['./my-chart.component.css']
 })
-export class MyChartComponent implements OnInit {
+export class MyChartComponent implements AfterViewChecked {
+
+  @ViewChild('graphElement')
+  private graphElement: ChartComponent;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewChecked() {
+    this.graphElement.chart.update();
   }
 
   @Input()
@@ -18,15 +23,22 @@ export class MyChartComponent implements OnInit {
   @Input()
   title = 'Titre';
 
+  @Input()
   data = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
       {
-        label: 'My First dataset',
+        label: 'First dataset',
         data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: 'Second dataset',
+        data: [55, 49, 70, 71, 66, 45, 50]
       }
     ]
   };
+
+  @Input()
   options = {
     responsive: true,
     maintainAspectRatio: false
